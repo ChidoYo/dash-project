8f87d98da2e7ece32c7c859ed35eba804cf319ee
Greg Kezos <greg.kezos@gmail.com>
2016-12-10 16:31:55 -0700

    filtered sort. need to fix bookmark and style

--- client/components/Bookmarks.js
+++ client/components/Bookmarks.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import $ from 'jquery';
 
-const Bookmark = () => (
- <div className="container">
-   <h3>Bookmarks</h3>
-   <p>Here are your bookmarks</p>
- </div>
-)
 
-export default Bookmark;
+class Bookmarks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { media: [] }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: '/api/media',
+      type: 'GET'
+    }).done( media => {
+      this.setState({ media });
+    });
+  }
+
+
+render() {
+  let filteredMedia = this.state.media.filter( medium => {
+    return medium.directory === "Bookmarks";
+  })
+
+  let media = filteredMedia.map( medium => {
+    return (
+      <li key={medium._id}>
+      {medium.title} {medium.directory}
+      </li>
+
+      )
+    });
+
+  return (
+
+      <div className="container">
+        <h3>Bookmarks</h3>
+        <p>Here are your bookmarks</p>
+        <ul>
+          {media}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+export default Bookmarks;
--- client/components/Videos.js
+++ client/components/Videos.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import $ from 'jquery';
 
-const Videos = () => (
- <div className="container">
-   <h3>Videos</h3>
-   <p>Here are your videos</p>
- </div>
-)
+
+class Videos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { media: [] }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: '/api/media',
+      type: 'GET'
+    }).done( media => {
+      this.setState({ media });
+    });
+  }
+
+
+render() {
+  let filteredMedia = this.state.media.filter( medium => {
+    return medium.directory === "Videos";
+  })
+
+  let media = filteredMedia.map( medium => {
+    return (
+      <li key={medium._id}>
+      {medium.title} {medium.directory}
+      </li>
+
+      )
+    });
+
+  return (
+
+      <div className="container">
+        <h3>Videos</h3>
+        <p>Here are your videos</p>
+        <ul>
+          {media}
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default Videos;
